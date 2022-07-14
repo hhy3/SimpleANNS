feda4eeda25fec160a59a57052d0faa184f9b47d
zh Wang <rekind133@outlook.com>
2022-07-14 12:26:59 +0800

    add InnerProduct dist.

--- src/Dist.hpp
+++ src/Dist.hpp
@@ -13,3 +13,16 @@ class L2Dist {
     return d;
   }
 };
+
+class InnerProductDist {
+ public:
+  auto operator()(const auto& x, const auto& y) {
+    assert(x.size() > 0 && x.size() == y.size());
+    auto d = decltype(x[0]){};
+    for (size_t i = 0; i < x.size(); ++i) {
+      d += x[i] * y[i];
+    }
+    return 1.0 - d;
+  }
+};
+
